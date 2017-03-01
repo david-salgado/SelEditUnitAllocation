04b6403159e450ff43f9e3a52a86ca1d17746b92
PC_Metodologia1 <david.salgado.fernandez@ine.es>
2017-03-01 08:45:56 +0100

    Correct file names

--- R/AllocatedUnits-class.R
+++ R/AllocatedUnits-class.R
@@ -1,25 +1,46 @@
 #' @title S4 class for the units allocated
 #'
-#' @description Definition of the S4 class named \code{AllocatedUnits} units allocated to be edited
-#' interactively in each population domain under the optimization approach to selective editing.
+#' @description Definition of the S4 class named \code{UnitAllocParam} for the parameters for
+#' the allocation of units to be edited interactively in each population domain under the
+#' optimization approach to selective editing.
 #'
 #' @slot Domains \linkS4class{data.table} with a row per domain with the values of the variables
 #' determining the domains of the data set.
 #'
-#' @slot Units list of \linkS4class{data.table}s with the statistical units per domain.
+#' @slot MaxAlloc Integer vector with the maximum number of units to be allocated in each domain.
 #'
+#' @slot MinAlloc Integer vector with the minimum number of units to be allocated in each domain.
+#'
+#' @slot AllocFactors List with numeric vectors as components containing the proportionality factor
+#' of each domain for the allocation.
+#'
+#' @slot MaxUnits Integer vector of length 1 with the maximal number of units to allocate.
 #'
 #' @examples
-#' new(Class = 'AllocatedUnits')
+#' UnitAllocParam <- new(Class = 'UnitAllocParam',
+#'                       MaxAlloc = as.integer(c(23, 23, 45, 23, 12, 23, 26, 76, 12, 1, 1, 1)),
+#'                       MinAlloc = rep(2L, length = 12),
+#'                       AllocFactors = list(c(0.43, 0.12, 0.234, 0.123, 0.934, 0.23, 0.754, 0.423, 0.923, 0.345, 0.512, 0.623),
+#'                                           c(0.43, 0.123, 0.34, 0.3, 0.34, 0.13, 0.954, 0.523, 0.223, 0.445, 0.522, 0.323)),
+#'                       DomainWeights = c(0.34, 0.12, 0.1, 1, 123, 0.12, 56, 123, 98, 12, 23, 87),
+#'                       MaxUnits = 265L )
 #'
 #' @import data.table
 #'
 #' @export
-setClass(Class = "AllocatedUnits",
+setClass(Class = "UnitAllocParam",
          slots = c(Domains = 'data.table',
-                   Units = 'list'),
+                   MaxAlloc = 'integer',
+                   MinAlloc = 'integer',
+                   AllocFactors = 'list',
+                   DomainWeights = 'numeric',
+                   MaxUnits = 'integer'),
          prototype = list(Domains = data.table::data.table(NULL),
-                          Units = list()),
+                          MaxAlloc = integer(0),
+                          MinAlloc = integer(0),
+                          AllocFactors = list(),
+                          DomainWeights = numeric(0),
+                          MaxUnits = integer(0)),
          validity = function(object){
 
 
